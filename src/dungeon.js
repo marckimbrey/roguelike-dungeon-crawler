eb77135c4c79e1d542d3ad6e26eadc0dbc34b3b5
marckimbrey <marckimbrey@gmail.com>
2016-11-19 21:28:24 +0000

    Refactor dungeon.js to revealing module pattern

--- src/dungeon.js
+++ src/dungeon.js
@@ -1,8 +1,5 @@
-const getRandom = (low, high) =>{
-    return~~ (Math.random() * (high - low)) + low;
-};
+let dungeon = (function(){
 
-export default function GenerateDungeon() {
   let dungeonMap = [];
   const mapSize = 60;
   let rooms = [];
@@ -11,39 +8,108 @@ export default function GenerateDungeon() {
   const minSize = 5;
   const maxSize = 15;
 
-  // create 2 dimentional array
-  for(let x=0; x<mapSize; x++) {
-    dungeonMap[x] = [];
-    for(let y=0; y<mapSize; y++) {
-        dungeonMap[x][y] = {
-          tile: 0,
-          explored: false,
-          enemy: false
-        };
+  function generate() {
+    // create 2 dimentional array
+    for(let x=0; x<mapSize; x++) {
+      dungeonMap[x] = [];
+      for(let y=0; y<mapSize; y++) {
+          dungeonMap[x][y] = {
+            tile: 0,
+            explored: false,
+            enemy: false
+          };
+      }
+    };
+    // add random sized rooms
+    for(let i=0; i<roomCount; i++) {
+      const room = generateRoom();
+      // if colllides with a room repeat loop
+      if(DoesCollide(room, rooms, i)) {
+        i--;
+        continue;
+      }
+      // to make sure rooms are not directly next ro each other
+      room.w--;
+      room.h--;
+
+      rooms.push(room);
+    };
+    // connect two random  rooms
+    for(let i=0; i<roomCount -1; i++) {
+      connectRooms(rooms[i], rooms[i+1]);
+    };
+
+    // set insides of rooms = 1
+    rooms.forEach((room) => {
+      for(let x=room.x; x<room.x +room.w; x++) {
+        for(let y=room.y; y<room.y +room.h; y++) {
+          dungeonMap[x][y].tile = 1
+        }
+      }
+    });
+
+    addWalls();
+
+    let player = false, boss = false;
+    const enemy1 = {
+      health: 20,
+      attack: 6
+    };
+    const enemy2 = {
+      health: 40,
+      attack: 8
     }
-  };
-  // add random sized rooms
-  for(let i=0; i<roomCount; i++) {
+    rooms.forEach((room) => {
+      let newVal;
+      if (!player) {
+        player = true;
+        newVal = fillRoom(room, 'player');
+      } else if (!boss) {
+        boss = true;
+        newVal = fillRoom(room, 'boss');
+      } else {
+        newVal = fillRoom(room);
+      }
+      switch (newVal[1]) {
+        case 'boss':
+          dungeonMap[newVal[0].x][newVal[0].y].enemy = 'boss';
+          break;
+        case 'enemy1':
+          dungeonMap[newVal[0].x][newVal[0].y].enemy = enemy1;
+          break;
+        case 'enemy2':
+          dungeonMap[newVal[0].x][newVal[0].y].enemy = enemy2;
+          break;
+        default:
+        break;
+      }
+      dungeonMap[newVal[0].x][newVal[0].y].tile = newVal[1];
+    });
+    return dungeonMap;
+  }
+
+  function generateRoom() {
     const room = {};
     room.x = getRandom(1, mapSize - maxSize - 1);
     room.y = getRandom(1, mapSize - maxSize - 1);
     room.w = getRandom(minSize, maxSize);
     room.h = getRandom(minSize, maxSize);
-    // if colllides with a room repeat loop
-    if(DoesCollide(room, rooms, i)) {
-      i--;
-      continue;
-    }
-    // to make sure rooms are not directly next ro each other
-    room.w--;
-    room.h--;
+    return room;
+  }
 
-    rooms.push(room);
-  };
-  // connect two random  rooms
-  for(let i=0; i<roomCount -1; i++) {
-    const roomA = rooms[i];
-    const roomB = rooms[i+1];
+  function DoesCollide(room, rooms, ignore) {
+    for(let i=0; i<rooms.length; i++) {
+      if (i === ignore) continue;
+      let check = rooms[i];
+      if (!((room.x + room.w < check.x) || (room.x > check.x + check.w) ||
+      (room.y + room.h < check.y) || (room.y > check.y + check.h))) return true;
+    }
+    return false
+  }
+
+  function connectRooms(room1, room2) {
+    const roomA = room1;
+    const roomB = room2;
 
     let pointA = {
       x: getRandom(roomA.x, roomA.x + roomA.w),
@@ -65,97 +131,64 @@ export default function GenerateDungeon() {
 
       dungeonMap[pointB.x][pointB.y].tile = 1;
     }
-  };
+  }
 
-  // set insides of rooms = 1
-  rooms.forEach((room) => {
-    for(let x=room.x; x<room.x +room.w; x++) {
-      for(let y=room.y; y<room.y +room.h; y++) {
-        dungeonMap[x][y].tile = 1
-      }
-    }
-  });
-  /*iterates through all the tiles in the map and if it finds a tile that is  =1
-  check all the surrounding tiles for empty values. If we find an empty tile
-  (that touches the floor) we build a wall =2.*/
-  for(let x=0; x<mapSize; x++) {
-    for(let  y=0; y<mapSize; y++) {
-      if (dungeonMap[x][y].tile === 1) {
-        for (let xx = x - 1; xx <= x + 1; xx++) {
-          for (let yy = y - 1; yy <= y + 1; yy++) {
-            if (dungeonMap[xx][yy].tile === 0) dungeonMap[xx][yy].tile = 2;
+  function addWalls() {
+    for(let x=0; x<mapSize; x++) {
+      for(let  y=0; y<mapSize; y++) {
+        if (dungeonMap[x][y].tile === 1) {
+          for (let xx = x - 1; xx <= x + 1; xx++) {
+            for (let yy = y - 1; yy <= y + 1; yy++) {
+              if (dungeonMap[xx][yy].tile === 0) dungeonMap[xx][yy].tile = 2;
+            }
           }
         }
       }
-    }
-  };
-  let player = false, boss = false;
-  const enemy1 = {
-    health: 20,
-    attack: 6
-  };
-  const enemy2 = {
-    health: 40,
-    attack: 8
+    };
   }
-  rooms.forEach((room) => {
-    let newVal;
-    if (!player) {
-      player = true;
-      newVal = fillRoom(room, 'player');
-    } else if (!boss) {
-      boss = true;
-      newVal = fillRoom(room, 'boss');
+
+  function fillRoom(room, char) {
+    const val = Math.random();
+    const randomSquare  = {
+      x: getRandom(room.x, room.x + room.w),
+      y: getRandom(room.y,  room.y + room.h)
+    };
+    if (char) {
+      return [randomSquare, char];
     } else {
-      newVal = fillRoom(room);
+      if (val < .4) {
+        return [randomSquare, 'enemy1']
+      } else if (val < .6) {
+        return [randomSquare, 'health']
+      } else if (val < .8) {
+        return [randomSquare, 'enemy2']
+      } else {
+            return [randomSquare, 'item']
+      }
     }
-    switch (newVal[1]) {
-      case 'boss':
-        dungeonMap[newVal[0].x][newVal[0].y].enemy = 'boss';
-        break;
-      case 'enemy1':
-        dungeonMap[newVal[0].x][newVal[0].y].enemy = enemy1;
-        break;
-      case 'enemy2':
-        dungeonMap[newVal[0].x][newVal[0].y].enemy = enemy2;
-        break;
-      default:
-      break;
-    }
-    dungeonMap[newVal[0].x][newVal[0].y].tile = newVal[1];
-  });
-  return dungeonMap;
-}
-
-function DoesCollide(room, rooms, ignore) {
-  for(let i=0; i<rooms.length; i++) {
-    if (i === ignore) continue;
-    let check = rooms[i];
-    if (!((room.x + room.w < check.x) || (room.x > check.x + check.w) ||
-    (room.y + room.h < check.y) || (room.y > check.y + check.h))) return true;
   }
-  return false
-}
 
-function fillRoom(room, char) {
-  const val = Math.random();
-  const randomSquare  = {
-    x: getRandom(room.x, room.x + room.w),
-    y: getRandom(room.y,  room.y + room.h)
+  function playerLocation() {
+    let playerLocation;
+    dungeonMap.forEach((row, x) => {
+      row.forEach((sqr, y) => {
+        if(sqr.tile === 'player'){
+          playerLocation =  {x: x, y: y};
+        }
+       });
+     });
+     return playerLocation;
+  }
+
+  function getRandom(low, high) {
+      return~~ (Math.random() * (high - low)) + low;
   };
-  if (char) {
-    return [randomSquare, char];
-  } else {
-    if (val < .4) {
-      return [randomSquare, 'enemy1']
-    } else if (val < .6) {
-      return [randomSquare, 'health']
-    } else if (val < .8) {
-      return [randomSquare, 'enemy2']
-    } else {
-          return [randomSquare, 'item']
-    }
-  }
 
+  return ({
+    generate: generate,
+    dungeonMap: dungeonMap,
+    playerLocation: playerLocation
+  })
+})();
 
-}
+export default dungeon;
