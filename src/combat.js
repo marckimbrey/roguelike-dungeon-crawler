76a742c7df85393ec0e0dfc10c7b74e96eadb468
marckimbrey <marckimbrey@gmail.com>
2017-03-08 20:01:02 +0000

    add attack and is killed functions

--- src/combat.js
+++ src/combat.js
@@ -1,25 +1,39 @@
 const combat = (function() {
-  function attack(x, y, enemy, player, dungeonMap) {
-    console.log(player.health, enemy.health);
-    let newDungeonMap = dungeonMap;
-    // calculate players damage
-    const playerDamage = _calculateDamage(player.attack + player.weapon.attack);
-    // calculate enemy damage
-    const enemyDamage = _calculateDamage(enemy.attack);
+  //  attack(destination, attacker, defender, dungeonMap)
+  // function attack(x, y, enemy, player, dungeonMap) {
+  //   console.log(player.health, enemy.health);
+  //   let newDungeonMap = dungeonMap;
+  //   // calculate players damage
+  //   const playerDamage = _calculateDamage(player.attack + player.weapon.attack);
+  //   // calculate enemy damage
+  //   const enemyDamage = _calculateDamage(enemy.attack);
+  //
+  //   // make attack
+  //   player.health -= enemyDamage;
+  //   enemy.health -= playerDamage;
+  //   if (player.health <= 0) { // if player is killed
+  //     alert("GAME OVER!!!");
+  //   } else if (enemy.health <= 0) { // if enemy is killed
+  //     newDungeonMap.tile = 1;
+  //     console.log("old health", player.health);
+  //     player = _addExerience(player, newDungeonMap[x][y].enemy.attack);
+  //     newDungeonMap[x][y].enemy = false;
+  //   }
+  //
+  //   return {dungeonMap: newDungeonMap, player: player}
+  // }
 
-    // make attack
-    player.health -= enemyDamage;
-    enemy.health -= playerDamage;
-    if (player.health <= 0) { // if player is killed
-      alert("GAME OVER!!!");
-    } else if (enemy.health <= 0) { // if enemy is killed
-      newDungeonMap.tile = 1;
-      console.log("old health", player.health);
-      player = _addExerience(player, newDungeonMap[x][y].enemy.attack);
-      newDungeonMap[x][y].enemy = false;
-    }
+  function attack(att, def) {
+    const damage =(att.weapon)?  _calculateDamage(att.attack + att.weapon.attack): _calculateDamage(att.attack);
+    console.log(def);
+    def.health -= damage;
+    console.log(def);
+    return (_defKilled(def))? null: def;
 
-    return {dungeonMap: newDungeonMap, player: player}
+  }
+
+  function _defKilled(def) {
+    if (def.health > 0) return true
   }
 
   function _addExerience(player, exp) {
@@ -39,7 +53,7 @@ const combat = (function() {
     return Math.round(Math.random() * ((attack + 3) - attack) + attack);
   }
   return ({
-    attack: attack
+    attack
   })
 })();
 
