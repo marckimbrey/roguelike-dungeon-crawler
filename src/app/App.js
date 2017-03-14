9fe95d6141b41538f9770967ea203f4b4977cc86
marckimbrey <marckimbrey@gmail.com>
2017-03-14 18:50:19 +0000

    enemiesTurn

--- src/app/App.js
+++ src/app/App.js
@@ -5,8 +5,8 @@ import dungeon from '../dungeon';
 import Canvas from '../Canvas/Canvas';
 import PlayerStats from '../PlayerStats/PlayerStats';
 import Combat from '../combat';
-import Movement from '../movement';
 import player from '../player';
+import enemy from '../enemy';
 
 class App extends Component {
   constructor(props) {
@@ -23,14 +23,26 @@ class App extends Component {
       enemies: dungeon.enemies
     }
     this.onArrowKeyPress = this.onArrowKeyPress.bind(this);
+    this.enemiesTurn = this.enemiesTurn.bind(this);
   }
   componentDidMount() {
     window.addEventListener("keydown", this.onArrowKeyPress);
+    setInterval(this.enemiesTurn, 5000);
   }
 
   onArrowKeyPress(event) {
     const newState = Object.assign(this.state, player.makeMove(event.keyCode,  this.state.player, this.state.dungeonMap));
     this.setState(newState);
+  }
+
+  enemiesTurn() {
+    this.state.dungeonMap.filter((row) => {
+      row.filter((row) => {
+        if(row.enemy) {
+          enemy.takeTurn(this.state.dungeonMap, this.state.player, row.enemy)
+        }
+      })
+    })
   }
 
   gameOver() {
